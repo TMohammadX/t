6fc52781f235665015741f97d2537aa5d78748f4
tmohammadx <bargineo@gmail.com>
2024-02-25 23:18:59 -0800

    C

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,6 +8,9 @@ export default function Home() {
     <main
       className={`grid place-items-center gap-10 h-screen w-screen text-center py-56  ${inter.className}`}
     >
+         <Head>
+        <title>RESTAURANT TOOSI</title>
+      </Head>
     <h1>WEBSITE IS UNDER CONSTRUCTION</h1>
     <img src="/logo.png" className="w-[150px] h-[150px] rounded"/>
     <h1>PLEASE NOTE THAT THERE IS ONLY ONE RESTAURANT TOOSI AND IS LOCATED AT 4686 BOUL DES SOURCES</h1>
